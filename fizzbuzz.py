903cb68447b33a4dcde0be14128cebe93f3e007f
PaymonJay <paymon.jalali@gmail.com>
2016-12-16 18:05:07 -0800

    Added comments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
-for i in range(1, 101):
-	if i%3 == 0 and i%5 == 0:
+for i in range(1, 101):		#for values 1 through 100
+	if i%3 == 0 and i%5 == 0:		#if value is divisible by both 3 and 5
 		print "fizzbuzz"
-	elif i%3 == 0:
+	elif i%3 == 0:		#if value is divisible by 3
 		print "fizz"
-	elif i%5 == 0:
+	elif i%5 == 0:		#if value is divisible by 5
 		print "buzz"
 	else:
 		print i
